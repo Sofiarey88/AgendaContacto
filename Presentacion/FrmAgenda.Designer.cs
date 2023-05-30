97d76e00e5c972ecc6b686f7ba01ab86fc90e6cd
Lautaro <lautyaraya65@gmail.com>
2023-05-30 14:58:30 -0300

    Agregar archivos de proyecto.

---
+++ Presentacion/FrmAgenda.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Presentacion
+{
+    partial class FrmAgenda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            label1 = new Label();
+            checkBox1 = new CheckBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            LsvListaContactos = new ListView();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(266, 60);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(132, 124);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(25, 113);
+            button1.Name = "button1";
+            button1.Size = new Size(154, 58);
+            button1.TabIndex = 1;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Contactos";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(49, 198);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(90, 24);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Favoritos";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(25, 257);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 24);
+            radioButton1.TabIndex = 4;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Boca";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(166, 257);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(63, 24);
+            radioButton2.TabIndex = 5;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "River";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // iconButton1
+            // 
+            iconButton1.BackColor = Color.LightGoldenrodYellow;
+            iconButton1.BackgroundImageLayout = ImageLayout.None;
+            iconButton1.ForeColor = SystemColors.ActiveCaptionText;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.DoorOpen;
+            iconButton1.IconColor = Color.IndianRed;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton1.Location = new Point(306, 564);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(121, 39);
+            iconButton1.TabIndex = 7;
+            iconButton1.Text = "Salir";
+            iconButton1.UseVisualStyleBackColor = false;
+            iconButton1.Click += iconButton1_Click;
+            // 
+            // LsvListaContactos
+            // 
+            LsvListaContactos.Location = new Point(42, 331);
+            LsvListaContactos.Name = "LsvListaContactos";
+            LsvListaContactos.Size = new Size(137, 191);
+            LsvListaContactos.TabIndex = 8;
+            LsvListaContactos.UseCompatibleStateImageBehavior = false;
+            LsvListaContactos.View = View.List;
+            LsvListaContactos.SelectedIndexChanged += LsvListaContactos_SelectedIndexChanged;
+            // 
+            // FrmAgenda
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(439, 615);
+            Controls.Add(LsvListaContactos);
+            Controls.Add(iconButton1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(checkBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            ForeColor = SystemColors.ControlText;
+            Margin = new Padding(3, 4, 3, 4);
+            MinimizeBox = false;
+            Name = "FrmAgenda";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Agenda de contactos";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label label1;
+        private CheckBox checkBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private ListView LsvListaContactos;
+    }
+}
